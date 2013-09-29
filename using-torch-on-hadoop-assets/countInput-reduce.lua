e9b9d87abad122831844267492bd215b6cd792bf
Roy Lowrance <roy.lowrance@gmail.com>
2013-09-29 18:01:52 -0400

    make tex's verbatim print nicer

--- using-torch-on-hadoop-assets/countInput-reduce.lua
+++ using-torch-on-hadoop-assets/countInput-reduce.lua
@@ -2,31 +2,31 @@
 ---- count number of records in standard in reduce function
 -- this is the reduce
 -- input is in some arbitrary order
---   ('records' --> <number of records>) ...
---   ('keyBytes' --> <number of bytes in keys>) ...
---   ('valueBytes' --> <number of bytes in values>) ...
+--   ("records" --> <number of records>) ...
+--   ("keyBytes" --> <number of bytes in keys>) ...
+--   ("valueBytes" --> <number of bytes in values>) ...
 -- output is total for each key value
 -- NOTE: keys are arbitary strings, not restricted to value named above
 
-require 'getKeyValue'
+require "getKeyValue"
 
 local lastKey = nil
 local count = 0
-for line in io.stdin:lines('*l') do
+for line in io.stdin:lines("*l") do
     if line == nil then break end
     local key, value = getKeyValue(line)
     if lastKey == key then
         count = count + tonumber(value)
     else
         if lastKey then
-            print(lastKey .. '\t' .. count)
+            print(lastKey .. "\t" .. count)
         end
         lastKey = key
         count = tonumber(value)
     end
 end
 if lastKey then 
-    print(lastKey .. '\t' .. count)
+    print(lastKey .. "\t" .. count)
 end
 
 
--- using-torch-on-hadoop-assets/getKeyValue.lua
+++ using-torch-on-hadoop-assets/getKeyValue.lua
@@ -6,7 +6,7 @@
 -- key   : string, bytes in s up to but not including the first tab (\t)
 -- value : string, bytes in s after the tab (\t); potentially empty
 function getKeyValue(s)
-    local key, value = string.match(s, '^(.*)\t(.*)$')
+    local key, value = string.match(s, "^(.*)\t(.*)$")
     if key == nil then
         return s, nil
     end
