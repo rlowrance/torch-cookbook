9d5ce5dfa892335cb36f341eaeee087546744510
Roy Lowrance <roy.lowrance@gmail.com>
2013-09-30 20:32:34 -0400

    first commit

---
+++ test-hadoop-job-assets/map-reduce-local.sh
@@ -0,0 +1,14 @@
+# run map-reduce locally
+
+# capture arguments
+INPUT=$1
+JOB=$2
+READLIMIT=$3
+
+# directory for local map-reduce output
+LOCAL_MAP_REDUCE_OUTPUT=map-reduce-output-local
+
+# do the work
+./$JOB-map.lua $READLIMIT < $INPUT | sort -k 1 | ./$JOB-reduce.lua > $LOCAL_MAP_REDUCE_OUTPUT/$INPUT.$JOB
+
+
