97fe5fef018b573fa016a612178548cc73c17b45
Roy Lowrance <roy.lowrance@gmail.com>
2013-09-30 20:51:37 -0400

    fold long lines so they print nicer

--- test-hadoop-job-assets/map-reduce-local.sh
+++ test-hadoop-job-assets/map-reduce-local.sh
@@ -9,6 +9,8 @@ READLIMIT=$3
 LOCAL_MAP_REDUCE_OUTPUT=map-reduce-output-local
 
 # do the work
-./$JOB-map.lua $READLIMIT < $INPUT | sort -k 1 | ./$JOB-reduce.lua > $LOCAL_MAP_REDUCE_OUTPUT/$INPUT.$JOB
+./$JOB-map.lua $READLIMIT < $INPUT | \
+  sort -k 1 | \
+  ./$JOB-reduce.lua > $LOCAL_MAP_REDUCE_OUTPUT/$INPUT.$JOB
 
 
